acfda951ad81bdc23565fc77c7f730c03da44f72
Mercier Laurent <mercier_laurent@live.fr>
2022-02-24 22:23:04 +0100

    try too https work starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/D3MenuScene';
 
+import "./themes/css/d3scene.css";
+import D3MenuScene from "./components/D3MenuScene";
+
+app.use(cors());
+
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('serverreact.key'),
+    cert: fs.readFileSync('serverreact.cert')};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="toto"></div>
+            toto
+            <div id="D3scene">
+<D3MenuScene id="D3scene"/>
+          </div>
+
+            <div>zfzef</div>
+            sdqdsqdsqsdsdqsd
+        </div>
+
+
+)
+    ;
 }
 
 export default App;
