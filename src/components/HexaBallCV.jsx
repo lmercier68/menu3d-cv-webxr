1f7c8d29a9b05569cc85557dc0ac1b14198c42db
Mercier Laurent <mercier_laurent@live.fr>
2022-02-26 00:41:55 +0100

    add img file for skybox (pc change)

--- src/components/HexaBallCV.jsx
+++ src/components/HexaBallCV.jsx
@@ -44,7 +44,43 @@ export default function Model({...props}) {
     const {nodes, materials} = useGLTF('./8B2.glb')
     const { controllers, player, isPresenting } = useXR();
 
-    const colorMap = useLoader(TextureLoader, 'PavingStones092_1K_Color.jpg')
+
+    const ft =useLoader(TextureLoader, './assets/bkg3_front5.png');
+    const bk = useLoader(TextureLoader, './assets/bkg3_back6.png');
+    const up = useLoader(TextureLoader, './assets/bkg3_top3.png');
+    const dn = useLoader(TextureLoader, './assets/bkg3_bottom4.png');
+    const rt =useLoader(TextureLoader, './assets/bkg3_right1.png');
+    const lf = useLoader(TextureLoader, './assets/bkg3_left2.png');
+
+    function createPathStrings(filename) {
+
+
+        const fileType = ".png";
+        const sides = [ft, bk, up, dn, rt, lf];
+        const pathStings = sides.map(side => {
+            return side;
+        });
+        return pathStings;
+    }
+    let skyboxImage = "purplenebula";
+    function createMaterialArray(filename) {
+        const skyboxImagepaths = createPathStrings(filename);
+        const materialArray = skyboxImagepaths.map(image => {
+            let texture = new THREE.TextureLoader().load(image);
+            return new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
+        });
+        return materialArray;
+    }
+    function init() {
+
+        const materialArray = createMaterialArray(skyboxImage);
+
+        skybox = new THREE.Mesh(skyboxGeo, materialArray);
+
+
+    }
+
+
     const skybox = useRef();
 
     player.position.set(0,0,10)
